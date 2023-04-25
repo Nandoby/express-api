88a92378492a57f8a65f9319634993214a8bc384
Biaccalli Ferdinando <nandobiaccalli@gmail.com>
2023-04-25 16:52:03 +0200

    auth et login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,23 +1,44 @@
-const authService = require("../services/auth.service")
-const { successResponse } = require("../utils/success.response")
+const authService = require("../services/auth.service");
+const userService = require("../services/user.service");
+const errorResponse = require("../utils/error.response");
+const { successResponse } = require("../utils/success.response");
 
 const authController = {
+  register: async (req, res) => {
+    const data = req.body;
+    // const user = await authService.register(data)
 
-    register: async (req, res) => {
-        const data = req.body
-        const user = await authService.register(data)
+    if (await userService.alreadyExist(data.email)) {
+      res.status(400).json(new errorResponse("L'email est déjà utilisée", 400));
+      return;
+    }
 
-        if (!user) {
-            res.sendStatus(400)
-            return
-        }
+    const user = await authService.register(data);
 
-        res.status(201).json(new successResponse(user, 200))
-    },
+    if (!album) {
+      res
+        .status(404)
+        .json(new errorResponse("L'email ou le mot de passe est invalide"));
+      return;
+    }
 
-    login: async (req, res) => {
-        res.sendStatus(501)
-    },
-}
+    res.status(201).json(new successResponse(user, 200));
+  },
 
-module.exports = authController
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await authService.login(email, password);
+
+    if (!user) {
+      res
+        .status(400)
+        .json(new errorResponse("email ou mot de passe invalide", 400));
+      return;
+    }
+
+    res.status(200).json(new successResponse(user, 200));
+  },
+};
+
+module.exports = authController;
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,27 +1,34 @@
-const argon2 = require('argon2')
-const db = require('../models')
-const UserDTO = require('../DTO/user.dto')
-const userService = require('./user.service')
+const argon2 = require("argon2");
+const db = require("../models");
+const UserDTO = require("../DTO/user.dto");
+const userService = require("./user.service");
 
 const authService = {
+  register: async (userToAdd) => {
+    const pwdHash = await argon2.hash(userToAdd.password);
 
-    register: async (userToAdd) => {
-        const pwdHash = await argon2.hash(userToAdd.password)
+    userToAdd.password = pwdHash;
 
-        userToAdd.password = pwdHash
-
-        if (await userService.alreadyExist(userToAdd.email)) {
-            return null;
-        }
-
-        const user = await db.User.create(userToAdd);
-
-        return user ? new UserDTO(user) : null
-    },
-
-    login: async () => {
-
+    if (await userService.alreadyExist(userToAdd.email)) {
+      return null;
     }
-}
 
-module.exports = authService
+    const user = await db.User.create(userToAdd);
+
+    return user ? new UserDTO(user) : null;
+  },
+
+  login: async (email, password) => {
+    const user = await db.User.findOne({ where: { email } });
+
+    if (!user) {
+      return null;
+    }
+
+    const isValidPassword = await argon2.verify(user.password, password);
+
+    return isValidPassword ? new UserDTO(user) : null
+  },
+};
+
+module.exports = authService;
